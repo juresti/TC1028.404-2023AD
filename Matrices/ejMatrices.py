75db52ad7d487708cb7e2f32463843c61f6acbc8
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2023-09-29 14:27:45 -0600

    Update ejMatrices.py

--- Matrices/ejMatrices.py
+++ Matrices/ejMatrices.py
@@ -13,8 +13,14 @@ def imprimeMatriz(matriz):
         for dato in renglon:
             salida += str(dato) + " "
         print(salida)
-    
-        
+ 
+def capturaMatriz(numRen,numCol):
+    """Va preguntando al usuario el dato en cada renglon
+        y columna de la matriz, hasta que esta llena la matriz.
+        ejemplo: "Dime el dato de la posicion 3,2: "
+    Regresa la matriz en lista de listas.
+    """
+    pass
 
 def tamanoMatriz(matriz):
     numRen = len(matriz)
