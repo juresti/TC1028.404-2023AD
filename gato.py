7b33d1a559a5a37f34e36ed32bb01985a84f9039
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2023-10-13 13:51:30 -0600

    Create gato.py

---
+++ gato.py
@@ -0,0 +1,38 @@
+def crearMatriz(numRen,numCol,inicializador):
+    matriz = []
+    for _ in range(numRen):
+        renglon = [inicializador] * numCol
+        matriz.append(renglon)
+    return matriz
+
+def imprimeMatriz(matriz):
+    for renglon in matriz:
+        salida = ""
+        for dato in renglon:
+            salida += dato + " "
+        print(salida)
+
+def preguntaUsuario(matriz):
+    print("\nEscoge donde vas a tirar")
+    imprimeMatriz(matriz)
+    ren = int(input("En que renglon quieres tirar? "))
+    col = int(input("En que columna quieres tirar? "))
+    return ren,col
+
+def asignaTiro(ren,col,ficha,tablero):
+    tablero[ren][col] = ficha
+    return tablero
+
+def gato():
+    tablero = crearMatriz(3,3,".")
+    print("Bienvenido al juego de gato")
+    imprimeMatriz(tablero)
+    
+    for num in range(9):
+        nRen,nCol = preguntaUsuario(tablero)
+        if (num % 2 == 0):
+            tablero = asignaTiro(nRen,nCol,"X",tablero)
+        else:
+            tablero = asignaTiro(nRen,nCol,"O",tablero)
+    
+gato()
